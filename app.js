9386571fcbc60f0b2ea59ae4b333e7f2d4417947
Auto-Ops Ltd <autoopsltd@outlook.com>
2018-05-25 19:50:49 +0100

    Broke code 4 to test sonarqube

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+man = 1;
+man2 = 2;
+man2 += man;
 var http = require('http');
 
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World!\n")
+  response.end("Hello World!\n");
 });
 
 server.listen(8080);
